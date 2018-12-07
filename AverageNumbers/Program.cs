70300041ac4e2640c4096a10f75f1e0dcb00097c
Ayshe Ali <aysheali2804@gmail.com>
2018-12-07 17:54:08 +0200

    Initial commit.

---
+++ AverageNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AverageNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
